500ee200a16b6322489f48ae168e5959f959d382
yjnoh1016 <yjnoh1016@naver.com>
2021-05-03 20:36:52 +0900

    210503 [UPDATE] SnapKit

--- iOS/SnapKit/snapkit_tutorial/snapkit_tutorial/ViewController.swift
+++ iOS/SnapKit/snapkit_tutorial/snapkit_tutorial/ViewController.swift
@@ -9,11 +9,78 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var greenBox = { () -> UIView in
+        let view = UIView()
+        view.backgroundColor = .green
+        return view
+    }()
+    
+    lazy var redBox = { () -> UIView in
+        let view = UIView()
+        view.backgroundColor = .red
+        return view
+    }()
+    
+    lazy var yellowBox = { () -> UIView in
+        let view = UIView()
+        view.backgroundColor = .yellow
+        return view
+    }()
+    
+    lazy var blueBox = { () -> UIView in
+        let view = UIView()
+        view.backgroundColor = .blue
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.addSubview(greenBox)
+        self.view.addSubview(redBox)
+        self.view.addSubview(yellowBox)
+        self.view.addSubview(blueBox)
+        
+        yellowBox.translatesAutoresizingMaskIntoConstraints = false
+        greenBox.translatesAutoresizingMaskIntoConstraints = false
+        redBox.translatesAutoresizingMaskIntoConstraints = false
+        blueBox.translatesAutoresizingMaskIntoConstraints = false
+        
+//        yellowBox.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8).isActive = true
+//        yellowBox.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.8).isActive = true
+//        yellowBox.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+//        yellowBox.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        yellowBox.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20).isActive = true
+        yellowBox.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20).isActive = true
+        yellowBox.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 20).isActive = true
+        yellowBox.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -20).isActive = true
     }
-
 
 }
 
+// Swift UI에서 UIViewController를 렌더링 한다.
+#if DEBUG
+import SwiftUI
+struct ViewControllerRepresentable: UIViewControllerRepresentable {
+    
+func updateUIViewController(_ uiView: UIViewController,context: Context) {
+        // leave this empty
+}
+@available(iOS 13.0.0, *)
+func makeUIViewController(context: Context) -> UIViewController{
+    ViewController()
+    }
+}
+@available(iOS 13.0, *)
+struct ViewControllerRepresentable_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ViewControllerRepresentable()
+                .ignoresSafeArea()
+                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
+                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+        }
+        
+    }
+} #endif
