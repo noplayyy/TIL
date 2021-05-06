b5310e2215abe27ba247a893e842233c46363508
yjnoh1016 <yjnoh1016@naver.com>
2021-05-06 15:53:47 +0900

    210506 [CREATE] Horizontal Collection View

---
+++ Swift/Horizontal CollectionView/cvcell/cvcell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cvcell
+//
+//  Created by 노연주 on 2021/05/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
