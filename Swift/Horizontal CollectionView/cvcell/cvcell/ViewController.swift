9ac98a53e8acb0a77a9187b6af070fa9a3fb89f4
yjnoh1016 <yjnoh1016@naver.com>
2021-05-07 09:16:24 +0900

    210507 [UPDATE] Horizontal collection view

--- Swift/Horizontal CollectionView/cvcell/cvcell/ViewController.swift
+++ Swift/Horizontal CollectionView/cvcell/cvcell/ViewController.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    fileprivate let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return cv
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .white
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 40).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 40).isActive = true
+
     }
 
 
