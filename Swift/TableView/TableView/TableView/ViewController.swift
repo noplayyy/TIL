b88cacdf64c5e5aa65151beea3d9373090094286
yjnoh1016 <yjnoh1016@naver.com>
2021-05-11 20:21:21 +0900

    210511 [UPDATE] TableView

--- Swift/TableView/TableView/TableView/ViewController.swift
+++ Swift/TableView/TableView/TableView/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func configureTableView() {
+        view.addSubview(tableView)
+        // set delegates
+        // set row height
+        // register cells
+    }
 }
-
