278a1cb16c073bc00500f87a33b8b7086b628b42
YeonJu <yjnoh1016@naver.com>
2021-06-26 21:14:49 +0900

    210626 [UPDATE] TableView

--- Swift/Storyboard/TableView/TableView/ViewController.swift
+++ Swift/Storyboard/TableView/TableView/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
 
 }
 
