f0c1d2568a4a49c322d08e4033dc44ca5ce6cc71
YeonJu <yjnoh1016@naver.com>
2021-07-06 18:19:47 +0900

    210706 [UPDATE] TextField and TextView Submit

--- Swift/Storyboard/TextField/TextField/ViewController.swift
+++ Swift/Storyboard/TextField/TextField/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var memo: UITextView!
+    @IBOutlet weak var pwTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configureUI()
     }
 
-
+    func configureUI(){
+        self.memo.layer.borderWidth = 1.0
+        self.memo.layer.borderColor = UIColor.black.cgColor
+        
+        self.pwTextField.isSecureTextEntry = true
+    }
+    
+    @IBAction func showAlert(_ sender: Any) {
+        let message = "제출 완료"
+        
+        let alert = UIAlertController(title: "Submit", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
