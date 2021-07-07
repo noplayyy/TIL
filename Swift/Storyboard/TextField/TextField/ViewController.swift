dfcb99c7607b68bbbdf56dfacfa069560545ef05
YeonJu <yjnoh1016@naver.com>
2021-07-07 20:39:29 +0900

    [UPDATE] empty

--- Swift/Storyboard/TextField/TextField/ViewController.swift
+++ Swift/Storyboard/TextField/TextField/ViewController.swift
@@ -12,11 +12,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var memo: UITextView!
     @IBOutlet weak var pwTextField: UITextField!
     
+    var empty: Bool!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         configureUI()
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+         self.view.endEditing(true)
+   }
 
     func configureUI(){
         self.memo.layer.borderWidth = 1.0
@@ -26,11 +31,30 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showAlert(_ sender: Any) {
-        let alert = UIAlertController(title: "Submit", message: "제출 완료", preferredStyle: .alert)
-        let action = UIAlertAction(title: "OK", style: .default)
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-        clearText()
+        emptyText()
+        if empty == true {
+            let alert = UIAlertController(title: "Submit", message: "제출 완료", preferredStyle: .alert)
+
+            let action = UIAlertAction(title: "OK", style: .default, handler: .none)
+                alert.addAction(action)
+                present(alert, animated: true, completion: nil)
+            clearText()
+        } else {
+            let errorAlert = UIAlertController(title: "Error", message: "ID와 PW를 모두 입력해주세요", preferredStyle: .alert)
+
+            let action = UIAlertAction(title: "OK", style: .destructive, handler: .none)
+                errorAlert.addAction(action)
+                present(errorAlert, animated: true, completion: nil)
+        }
+    }
+    
+
+    func emptyText(){
+        if idTextField.text?.isEmpty == false && pwTextField.text?.isEmpty == false {
+            empty = true
+        } else {
+            empty = false
+        }
     }
     
     func clearText(){
