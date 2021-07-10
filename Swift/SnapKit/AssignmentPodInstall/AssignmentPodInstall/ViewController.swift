aad8cb538106aaa376a6a1a03a881aeb9a721e9a
YeonJu <yjnoh1016@naver.com>
2021-07-11 00:24:08 +0900

    210711 [UPDATE] add text

--- Swift/SnapKit/AssignmentPodInstall/AssignmentPodInstall/ViewController.swift
+++ Swift/SnapKit/AssignmentPodInstall/AssignmentPodInstall/ViewController.swift
@@ -7,18 +7,36 @@
 
 import UIKit
 import SnapKit
+import Then
 
 class ViewController: UIViewController {
-
+    
+    
+    lazy var text = UILabel().then {
+        $0.text = "TEXT"
+        $0.textColor = .red
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
     }
-
+    
+    func configureUI(){
+        view.backgroundColor = .white
+        
+        view.addSubview(text)
+        
+        text.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+    
 }
 
 
-//MARK: - Preview
+//MARK: - Preview [ SwiftUI를 이용한 미리보기 코드 ]
+
 #if DEBUG
 import SwiftUI
 struct ViewControllerRepresentable: UIViewControllerRepresentable {
