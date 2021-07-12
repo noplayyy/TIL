13f2c7f5a8c6f720d754b2248d6ffadabe3e3189
YeonJu <yjnoh1016@naver.com>
2021-07-12 20:06:31 +0900

    210712 [UPDATE]

--- Swift/Storyboard/WebView/WebView/ViewController.swift
+++ Swift/Storyboard/WebView/WebView/ViewController.swift
@@ -30,28 +30,28 @@ class ViewController: UIViewController, WKUIDelegate,WKNavigationDelegate {
         webView.load(request)
     }
 
-    override func didReceiveMemoryWarning() { super.didReceiveMemoryWarning() } //모달 창이 닫힐 때 앱 종료 현상 방지
-    
-    //alert 처리
-    func webView(_webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void){
-        let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { (action) in completionHandler() }))
-        self.present(alertController, animated: true, completion: nil)
-    }
-    
-    //confirm 처리
-    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
-        let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(title: "취소", style: .default, handler: { (action) in completionHandler(false) }))
-        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { (action) in completionHandler(true) }))
-        self.present(alertController, animated: true, completion: nil)
-    }
-        
-    // href = "_blank" 처리
-    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
-            if navigationAction.targetFrame == nil { webView.load(navigationAction.request) }
-            return nil
-    }
+//    override func didReceiveMemoryWarning() { super.didReceiveMemoryWarning() } //모달 창이 닫힐 때 앱 종료 현상 방지
+//
+//    //alert 처리
+//    func webView(_webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void){
+//        let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
+//        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { (action) in completionHandler() }))
+//        self.present(alertController, animated: true, completion: nil)
+//    }
+//
+//    //confirm 처리
+//    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+//        let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
+//        alertController.addAction(UIAlertAction(title: "취소", style: .default, handler: { (action) in completionHandler(false) }))
+//        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { (action) in completionHandler(true) }))
+//        self.present(alertController, animated: true, completion: nil)
+//    }
+//
+//    // href = "_blank" 처리
+//    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+//            if navigationAction.targetFrame == nil { webView.load(navigationAction.request) }
+//            return nil
+//    }
 
 }
 
