df82773b18b8b151be8a7df31765e5a919a1c86d
YeonJu <yjnoh1016@naver.com>
2021-07-19 10:38:43 +0900

    210719 [UPDATE]

---
+++ Swift/Storyboard/CustomCollectionView/CustomCollectionView/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  CustomCollectionView
+//
+//  Created by 노연주 on 2021/07/19.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var collectionViewLabel: UILabel!
+    
+}
+
--- Swift/Storyboard/CustomCollectionView/CustomCollectionView/ViewController.swift
+++ Swift/Storyboard/CustomCollectionView/CustomCollectionView/ViewController.swift
@@ -7,13 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
+    var collectionItems = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setCollectionItem()
     }
 
+    func setCollectionItem(){ NSLog("setCollectionItem")
+        collectionItems = [
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Five",
+            "Six",
+            "Seven",
+            "Eight",
+            "Nine",
+            "Ten"
+        ]
+    }
+    
+    // MARK: - CollectionView DataSource
 
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return collectionItems.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath) as! CollectionViewCell
+        cell.collectionViewLabel.text = collectionItems[indexPath.row]
+        return cell
+    }
 }
 
