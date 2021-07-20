3aa8877e3a4e154bceb6e21eee7e392eff495740
YeonJu <yjnoh1016@naver.com>
2021-07-20 20:33:53 +0900

    210720 [UPDATE] View Animate

--- Swift/Storyboard/ViewAnimate/ViewAnimate/ViewController.swift
+++ Swift/Storyboard/ViewAnimate/ViewAnimate/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var extendView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapOnButton(_ sender: Any) {
+        UIView.animate(withDuration: 3, delay: 0.1, options: .curveEaseInOut, animations: { () -> Void in self.extendView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 500)
+        },
+        completion: { (didFinish) -> Void in })
+    }
+    
 }
 
