df40b4f0e3dff661b026440becc0b022c7bc7c7d
YeonJu <yjnoh1016@naver.com>
2021-07-22 10:50:36 +0900

    210722 [UPDATE] ImageLayer

--- Swift/Storyboard/ImageViewLayer/ImageViewLayer/ViewController.swift
+++ Swift/Storyboard/ImageViewLayer/ImageViewLayer/ViewController.swift
@@ -21,10 +21,11 @@ class ViewController: UIViewController {
         image.contentMode = .scaleAspectFit
         
         // shadow
-        image.layer.shadowOffset = CGSize(width: 5, height: 5)
-        image.layer.shadowOpacity = 0.7
-        image.layer.shadowRadius = 5
-        image.layer.shadowColor = UIColor.gray.cgColor
+        image.layer.masksToBounds = false // 내부에 속한 요소들이 UIView 밖을 벗어날 때, 잘라낼 것인지. 그림자는 밖에 그려지는 것이므로 false 로 설정
+        image.layer.shadowOffset = CGSize(width: 0, height: 0) // 그림자 위치
+        image.layer.shadowOpacity = 0.7 // 그림자 투명도 설정 (MIN = 0 ~ MAX = 1)
+        image.layer.shadowRadius = 10 // 그림자 경계 선명도 설정
+        image.layer.shadowColor = UIColor.gray.cgColor // 그림자 색상
         
         // 넘어간 부분 잘라내기 - 인스펙터 창에서 설정 가능
 //        image.clipsToBounds = true
