6aab7cc47051e3c9708349e7b2d843f854ec8a67
YeonJu <yjnoh1016@naver.com>
2021-08-01 13:03:55 +0900

    210801 [UPDATE] ScrollView

--- Swift/ScrollView/ScrollView/ViewController.swift
+++ Swift/ScrollView/ScrollView/ViewController.swift
@@ -20,6 +20,16 @@ class ViewController: UIViewController {
         let scrollView =  UIScrollView(frame: CGRect(x: 10, y: 10, width: view.frame.size.width - 20, height: view.frame.size.height - 20))
         scrollView.backgroundColor = .green
         view.addSubview(scrollView)
+        
+        let topButton = UIButton(frame: CGRect(x: 20, y: 20, width: 100, height: 100))
+        topButton.backgroundColor = .blue
+        scrollView.addSubview(topButton)
+        
+        let bottomButton = UIButton(frame: CGRect(x: 20, y: 2000, width: 100, height: 100))
+        topButton.backgroundColor = .blue
+        scrollView.addSubview(bottomButton)
+        
+        scrollView.contentSize = CGSize(width: self.view.frame.width, height: 2200)
     }
 
 }
