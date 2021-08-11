d1d853a274f0860a9c51b2eea55c039b3b212e8a
YeonJu <yjnoh1016@naver.com>
2021-08-11 23:46:16 +0900

    210811 [CREATE]

---
+++ Swift/FirstProgrammaticallyAssignment/FirstProgrammaticallyAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstProgrammaticallyAssignment
+//
+//  Created by 노연주 on 2021/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
